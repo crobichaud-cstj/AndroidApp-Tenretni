a1fc27fb19654d07deff8b0d670674ec542db960
Zakaria Mrad <1839783@cstj.qc.ca>
2023-04-25 11:53:21 -0400

    Started loading_activity

---
+++ app/src/main/java/ca/qc/cstj/tptenretni/ui/loading/LoadingActivity.kt
@@ -0,0 +1,13 @@
+package ca.qc.cstj.tptenretni.ui.loading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.qc.cstj.tptenretni.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+    }
+}
