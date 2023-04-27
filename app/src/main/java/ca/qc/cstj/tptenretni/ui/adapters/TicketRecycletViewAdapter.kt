4bc32cf7c615a05e0b460097553757b4b7f193ed
2150658 <2150658@cstj.qc.ca>
2023-04-27 09:39:22 -0400

    Added detailTicket and argument

---
+++ app/src/main/java/ca/qc/cstj/tptenretni/ui/adapters/TicketRecycletViewAdapter.kt
@@ -0,0 +1,40 @@
+package ca.qc.cstj.tptenretni.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ca.qc.cstj.tptenretni.R
+import ca.qc.cstj.tptenretni.databinding.ItemTicketBinding
+import ca.qc.cstj.tptenretni.models.Ticket
+import com.bumptech.glide.Glide
+
+class TicketRecycletViewAdapter(var tickets: List<Ticket>,
+                                private val onTicketClick: (Ticket)->Unit
+) : RecyclerView.Adapter<TicketRecycletViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_ticket, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val ticket = tickets[position]
+        holder.bind(ticket)
+
+        holder.itemView.setOnClickListener{
+            onTicketItemClick(ticket)
+        }
+    }
+
+    override fun getItemCount() = tickets.size
+
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ItemTicketBinding.bind(view)
+
+        fun bind(ticket: Ticket) {
+
+
+        }
+    }
+}
---
+++ app/src/main/java/ca/qc/cstj/tptenretni/ui/tickets/DetailTicketViewModel.kt
@@ -0,0 +1,7 @@
+package ca.qc.cstj.tptenretni.ui.tickets
+
+import androidx.lifecycle.ViewModel
+
+class DetailTicketViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
