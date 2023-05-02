246d3fcec4b2c89002c0bbc5778a307c98e88e5b
Ludovic Fournier <fouludovic@gmail.com>
2023-05-02 16:20:36 -0400

    reste le drapeau et les gatways a afficher

--- app/src/main/java/ca/qc/cstj/tptenretni/ui/tickets/detail/DetailTicketFragment.kt
+++ app/src/main/java/ca/qc/cstj/tptenretni/ui/tickets/detail/DetailTicketFragment.kt
@@ -9,8 +9,11 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
 import ca.qc.cstj.tptenretni.R
+import ca.qc.cstj.tptenretni.core.ColorHelper
 import ca.qc.cstj.tptenretni.core.Constants
+import ca.qc.cstj.tptenretni.core.DateHelper
 import ca.qc.cstj.tptenretni.databinding.FragmentDetailTicketBinding
 import ca.qc.cstj.tptenretni.models.Gateway
 import ca.qc.cstj.tptenretni.models.Ticket
@@ -22,12 +25,15 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import ca.qc.cstj.tptenretni.ui.adapters.GatewayRecyclerViewAdapter
 
 class DetailTicketFragment : Fragment(R.layout.fragment_detail_ticket) {
     private val args: TicketsFragmentArgs by navArgs()
 
     private var position : LatLng? = null
     private lateinit var customerName : String
+
+    private lateinit var gatewayRecyclerViewAdapter: GatewayRecyclerViewAdapter
 
     private val binding: FragmentDetailTicketBinding by viewBinding()
     private val viewModel: DetailTicketViewModel by viewModels {
@@ -59,13 +65,19 @@ class DetailTicketFragment : Fragment(R.layout.fragment_detail_ticket) {
                     requireActivity().supportFragmentManager.popBackStack()
                 }
 
-                DetailTicketUiState.Loading -> Unit
+                DetailTicketUiState.Loading -> {
+
+                }
                 is DetailTicketUiState.SuccessTicket -> {
                     ticket = it.ticket
-                    binding.txvHref.text = it.ticket.href
-                    binding.txvHref.text=it.ticket.href
+                    binding.chpTicketStatus.text = ticket.status
+                    binding.chpTicketPriority.text = ticket.priority
+                    binding.chpTicketPriority.chipBackgroundColor = ColorHelper.ticketPriorityColor(binding.root.context, ticket.priority)
+                    binding.chpTicketStatus.chipBackgroundColor = ColorHelper.ticketStatusColor(binding.root.context, ticket.status)
+                    binding.txvTicketDate.text = DateHelper.formatISODate(ticket.createdDate)
+                    binding.txvTicketId.text = binding.root.context.getString(R.string.ticket_id, ticket.ticketNumber)
 
-                    when(Constants.TicketStatus.valueOf(it.ticket.status)){
+                    when(Constants.TicketStatus.valueOf(ticket.status)){
                         Constants.TicketStatus.Open -> statusOpen()
                         Constants.TicketStatus.Solved -> statusSolve()
                     }
@@ -74,23 +86,29 @@ class DetailTicketFragment : Fragment(R.layout.fragment_detail_ticket) {
 
                 is DetailTicketUiState.SuccessCustomer -> {
                     ticket.customer = it.customer
-                    binding.textView2.text = it.customer.href
+                    binding.txvNom.text = it.customer.firstName + " " + it.customer.lastName
+                    binding.txvVille.text = it.customer.city
+                    binding.txvAdresse.text = it.customer.address
+                    binding.txvPays.text = it.customer.country
                     customerName = it.customer.firstName + " " + it.customer.lastName
                     position = LatLng(it.customer.coord.latitude.toDouble(), it.customer.coord.longitude.toDouble())
                 }
 
+                is DetailTicketUiState.SuccessGateways -> {
+                    binding.rcvGateways.visibility = View.VISIBLE
+                    gatewayRecyclerViewAdapter.gateways = it.Gateways
+                    gatewayRecyclerViewAdapter.notifyDataSetChanged()
+                }
+
                 is DetailTicketUiState.SuccessGateway -> {
-                    binding.txvHref.text = it.gateway.serialNumber
+
                 }
             }
         }.launchIn(viewLifecycleOwner.lifecycleScope)
 
         binding.btnOpen.setOnClickListener{
-
             viewModel.changeStatus(Constants.NameStatus.OPEN)
             statusOpen()
-
-
         }
 
         binding.btnSolve.setOnClickListener{
@@ -104,10 +122,16 @@ class DetailTicketFragment : Fragment(R.layout.fragment_detail_ticket) {
                 findNavController().navigate(action)
             }
         }
+
+        binding.rcvGateways.apply {
+            layoutManager = GridLayoutManager(requireContext(),Constants.RecyclerAdapters.Gateways)
+            adapter = gatewayRecyclerViewAdapter
+
+        }
     }
 
     private fun statusOpen(){
-        binding.btnOpen.visibility = View.GONE
+        binding.btnOpen.visibility = View.INVISIBLE
         binding.btnInstall.visibility = View.VISIBLE
         binding.btnSolve.visibility = View.VISIBLE
     }
--- app/src/main/java/ca/qc/cstj/tptenretni/ui/tickets/detail/DetailTicketViewModel.kt
+++ app/src/main/java/ca/qc/cstj/tptenretni/ui/tickets/detail/DetailTicketViewModel.kt
@@ -6,8 +6,10 @@ import androidx.lifecycle.viewModelScope
 import ca.qc.cstj.tptenretni.core.ApiResult
 import ca.qc.cstj.tptenretni.data.repositories.CustomerRepository
 import ca.qc.cstj.tptenretni.data.repositories.TicketRepository
+import ca.qc.cstj.tptenretni.data.repositories.GatewayRepository
 import ca.qc.cstj.tptenretni.models.Gateway
 import ca.qc.cstj.tptenretni.models.Ticket
+import ca.qc.cstj.tptenretni.ui.gateways.GatewaysUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -16,6 +18,7 @@ import kotlinx.coroutines.launch
 class DetailTicketViewModel(private val href: String) : ViewModel() {
     private val ticketRepository = TicketRepository()
     private val customerRepository = CustomerRepository()
+    private val gatewayRepository = GatewayRepository()
 
     private val _detailTicketUiState =
         MutableStateFlow<DetailTicketUiState>(DetailTicketUiState.Loading)
@@ -30,7 +33,8 @@ class DetailTicketViewModel(private val href: String) : ViewModel() {
                         ApiResult.Loading -> DetailTicketUiState.Loading
                         is ApiResult.Success -> {
                             loadCustomer(apiResult.data.customer.href)
-                            DetailTicketUiState.SuccessTicket(apiResult.data);
+                            DetailTicketUiState.SuccessTicket(apiResult.data)
+
                         }
                     }
                 }
@@ -78,6 +82,20 @@ class DetailTicketViewModel(private val href: String) : ViewModel() {
                         is ApiResult.Error -> DetailTicketUiState.Error(apiResult.exception)
                         ApiResult.Loading -> DetailTicketUiState.Loading
                         is ApiResult.Success -> DetailTicketUiState.SuccessGateway(apiResult.data)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun refreshGateways() {
+        viewModelScope.launch {
+            gatewayRepository.retrieveAll().collect {
+                _detailTicketUiState.update { _ ->
+                    when (it) {
+                        is ApiResult.Error -> DetailTicketUiState.Error(it.exception)
+                        ApiResult.Loading -> DetailTicketUiState.Loading
+                        is ApiResult.Success -> DetailTicketUiState.SuccessGateways(it.data)
                     }
                 }
             }
